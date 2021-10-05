18022b09232ca7670cee8a9990c463b6b5d138a3
Suharda Silva <suhardapo@gmail.com>
2021-10-05 17:48:43 +0530

    First Python program added

---
+++ numbers.py
@@ -0,0 +1,9 @@
+# A simple function to add two numbers together
+
+def numplay(a,b):
+    '''Pass two to get the addition of those two numbers '''
+    return a+b
+
+if __name__ == '__main__':
+    print(numplay(10,12))
+    
