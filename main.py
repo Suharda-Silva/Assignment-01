8950d53879aa9b6f3058d9290641237c56eac6b9
Suharda Silva <suhardapo@gmail.com>
2021-10-06 21:26:29 +0530

    main.py was added

---
+++ main.py
@@ -0,0 +1,8 @@
+from numbers import numplay
+
+firstNum = int(input('Enter your first number: '))
+secondNum = int(input('Enter your first number: '))
+
+result = numplay(firstNum,secondNum)
+
+print(f'The addition of {firstNum} and {secondNum} = {result}')
